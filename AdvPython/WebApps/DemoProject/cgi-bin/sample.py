8658f95e5bad0e6e53405d98ede476fd1216b65e
Emanul <156309215+Emzy007@users.noreply.github.com>
2024-05-23 06:45:53 +0000

    v0.2

--- AdvPython/WebApps/DemoProject/cgi-bin/sample.py
+++ AdvPython/WebApps/DemoProject/cgi-bin/sample.py
@@ -1,5 +1,9 @@
-#!/home/codespace/.python/current/bin/python
-import cgi
+#!/usr/bin/env python3
+import cgi, cgitb
+#CGITB - CGI Traceback which displays runtime error on the webpage. Shows error type with location.
+#If a file name is given then it will not show the error on webpage else output to a file
+cgitb.enable(display=0, logdir='AdvPython/WebApps/DemoProject/demoprojlog')
+
 print('Content-type:text/html \n')
 print('''
 	<html>
